8a29e3c3411a28788f4c3aa3d99227c0c99a8626
Goutham-Badhrinath <gouthambadhrinath123@gmail.com>
2026-06-24 13:43:15 +0530

    LSU fixed and testbench for external mem access added

--- rtl/CoreRV.sv
+++ rtl/CoreRV.sv
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 module CoreRV #(
     parameter BOOT_ADDR = 32'h0000_0000,
-    parameter INSTR_ADDR = 32'h0000_0400,
-    parameter DATA_ADDR = 32'h0001_0000,
-    parameter EXTERNAL_ADDR = 32'h0002_0000
+    parameter INSTR_ADDR = 32'h0000_0100,
+    parameter DATA_ADDR = 32'h0000_0200,
+    parameter EXTERNAL_ADDR = 32'h0000_0300
 )(
     input logic clk,
     input logic rst,
@@ -60,8 +60,8 @@ logic M_over;
 // Buffer to hold Stage 1 outputs for use in Stage 2
 s1_buffer s1_buf;
 
-assign PCSrc = {Jump,branch_flush};
-assign redirect_flush = branch_flush | Jump;
+assign PCSrc = (m_stall) ? 2'b00 : {Jump,branch_flush};
+assign redirect_flush = (!m_stall) && (branch_flush | Jump);
 assign front_stall = m_stall | load_hazard;
 logic loadE;
 assign loadE = s1_buf.ctrl[17] && (s1_buf.ctrl[16:15] == 2'b01); // Check if it's a load instruction in EX stage
@@ -119,6 +119,10 @@ logic [4:0] rd_buf;
 logic       regwrite_buf;
 logic [31:0] Mem_Ctrl_PC;
 logic boot_mode;
+
+//LSU
+logic lsu_req_pulse;
+logic lsu_req_issued;
 
 Memory_Ctrl #(
     .INSTR_ADDR(INSTR_ADDR)
@@ -271,7 +275,7 @@ always_comb begin
     if(exec_result >= INSTR_ADDR && exec_result < DATA_ADDR)  begin
         // Handle instruction memory access
         mem_select = 2'b01;
-        en_mem = ctrl_s2[4] && boot_mode; // Only enable instruction memory access in boot mode
+        en_mem = ctrl_s2[4] && boot_mode && !m_stall; // Only enable instruction memory access in boot mode
         addr_mem = (exec_result - INSTR_ADDR) >> 2;
         unique case (ctrl_s2[2:1])
             2'b00: wdata_mem = rs2_value_S23; //word
@@ -295,7 +299,7 @@ always_comb begin
             end
             default: wdata_mem = rs2_value_S23;
         endcase
-        we_mem = ctrl_s2[3];
+        we_mem = ctrl_s2[3] && !m_stall;
         sign_ext_mem = ctrl_s2[0];
         unique case (ctrl_s2[2:1])
             2'b00: be_mem = 4'b1111; //word
@@ -322,7 +326,7 @@ always_comb begin
     end
     else if(exec_result >= DATA_ADDR && exec_result < EXTERNAL_ADDR) begin
         mem_select = 2'b00;
-        dram_en_i = ctrl_s2[4];
+        dram_en_i = ctrl_s2[4] && !m_stall;
         dram_addr_i = (exec_result - DATA_ADDR) >> 2;
         unique case (ctrl_s2[2:1])
             2'b00: dram_wdata_i = rs2_value_S23; //word
@@ -346,7 +350,7 @@ always_comb begin
             end
             default: dram_wdata_i = rs2_value_S23;
         endcase
-        dram_we_i = ctrl_s2[3];
+        dram_we_i = ctrl_s2[3] && !m_stall;
         dram_sign_ext_i = ctrl_s2[0];
         unique case (ctrl_s2[2:1])
             2'b00: dram_be_i = 4'b1111; //word
@@ -379,7 +383,7 @@ always_comb begin
         lsu_wdata_i = rs2_value_S23;
         lsu_sign_ext_i = ctrl_s2[0];
         lsu_req_i = ctrl_s2[4];
-        adder_result_ex_i = exec_result;
+        adder_result_ex_i = (exec_result - EXTERNAL_ADDR);
     end
 end
 
@@ -424,16 +428,16 @@ LSU_RVX lsu(
   .data_rdata_i(data_rdata_i),
 
   // ID/EX inputs
-  .lsu_we_i(lsu_we_i),
-  .lsu_type_i(lsu_type_i),
-  .lsu_wdata_i(lsu_wdata_i),
-  .lsu_sign_ext_i(lsu_sign_ext_i),
-  .lsu_req_i(lsu_req_i),
-  .adder_result_ex_i(adder_result_ex_i),
+  .we_i(lsu_we_i),
+  .type_i(lsu_type_i),
+  .wdata_i(lsu_wdata_i),
+  .sign_ext_i(lsu_sign_ext_i),
+  .req_i(lsu_req_pulse),
+  .addr_i(adder_result_ex_i),
 
   // outputs to WB / pipeline control
-  .lsu_rdata_o(lsu_rdata_o),
-  .lsu_rdata_valid_o(lsu_rdata_valid_o),
+  .rdata_o(lsu_rdata_o),
+  .rvalid_o(lsu_rdata_valid_o),
   .busy_o(busy_o)
 );
 
@@ -480,13 +484,31 @@ always_ff @(posedge clk) begin
     end
 end
 logic [31:0] mem_data_wb;
+/////////////////////////////////////////////////////////////
+// LSU waiting signals for external memory access
+logic lsu_load_wait_wb;
+assign lsu_load_wait_wb = (mem_select_wb == 2'b10) &&
+                          (memtoreg_wb == 2'b01) &&
+                          !lsu_rdata_valid_o;
+
+assign lsu_req_pulse = lsu_req_i && !lsu_req_issued;
+
+always_ff @(posedge clk) begin
+    if (rst)
+        lsu_req_issued <= 1'b0;
+    else if (lsu_rdata_valid_o)
+        lsu_req_issued <= 1'b0;
+    else if (lsu_req_pulse)
+        lsu_req_issued <= 1'b1;
+end
+/////////////////////////////////////////////////////////////
 
 always_comb begin
     mem_data_wb = 32'd0;
     case(mem_select_wb)
         2'b00: mem_data_wb = dram_rdata_i;
         2'b01: mem_data_wb = rdata_mem;
-        2'b10: mem_data_wb = lsu_rdata_valid_o ? lsu_rdata_o : 32'd0;
+        2'b10: mem_data_wb = lsu_rdata_o;
         default: mem_data_wb = 32'd0;
     endcase
 end
@@ -500,10 +522,18 @@ always_ff @(posedge clk) begin
         rd_buf          <= 5'd0;
         regwrite_buf    <= 1'b0;
     end
+    else if (lsu_load_wait_wb) begin
+        alu_result_buf  <= alu_result_buf;
+        pc_buf          <= pc_buf;
+        memtoreg_buf    <= memtoreg_buf;
+        mem_data_buf    <= mem_data_buf;
+        rd_buf          <= rd_buf;
+        regwrite_buf    <= regwrite_buf;
+    end
     else begin
-        alu_result_buf <= alu_result_wb;
-        pc_buf         <= pc_wb;
-        memtoreg_buf   <= memtoreg_wb;
+        alu_result_buf  <= alu_result_wb;
+        pc_buf          <= pc_wb;
+        memtoreg_buf    <= memtoreg_wb;
         mem_data_buf    <= mem_data_wb;
         rd_buf          <= rd_wb;
         regwrite_buf    <= regwrite_wb;
